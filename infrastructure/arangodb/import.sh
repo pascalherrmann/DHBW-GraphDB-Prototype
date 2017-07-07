649ab86ba3613e7821e33c1d36e826e6318b57e3
Pascal Herrmann <pasher1@web.de>
2017-07-07 11:59:35 +0200

    added arangodb dockerfile

---
+++ infrastructure/arangodb/import.sh
@@ -0,0 +1,7 @@
+bigEcho () {
+   echo "=================================================="
+   echo $1
+   echo "=================================================="
+}
+
+bigEcho "Hallo und Herzlich Willkommen zum Wikipedia-Import bei ARRANGODB!"

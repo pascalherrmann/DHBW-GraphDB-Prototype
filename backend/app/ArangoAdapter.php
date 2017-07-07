18171a313b636e2c643c8e5c594aed9937934931
henp95 <wi14153@lehre.dhbw-stuttgart.de>
2017-07-07 11:46:27 +0200

    start neo4 adapter

--- backend/app/ArangoAdapter.php
+++ backend/app/ArangoAdapter.php
@@ -16,6 +16,37 @@ use triagens\ArangoDb\Statement as Statement;
 class ArangoAdapter implements WikiInterface
 {
 
+    protected $connection;
+
+    /**
+     * ArangoAdapter constructor.
+     */
+    public function __construct()
+    {
+        $connectionOptions = array(
+            // server endpoint to connect to
+            ConnectionOptions::OPTION_ENDPOINT => 'tcp://127.0.0.1:8529',
+            // authorization type to use (currently supported: 'Basic')
+            ConnectionOptions::OPTION_AUTH_TYPE => 'Basic',
+            // user for basic authorization
+            ConnectionOptions::OPTION_AUTH_USER => 'root',
+            // password for basic authorization
+            ConnectionOptions::OPTION_AUTH_PASSWD => '',
+            // connection persistence on server. can use either 'Close' (one-time connections) or 'Keep-Alive' (re-used connections)
+            ConnectionOptions::OPTION_CONNECTION => 'Close',
+            // connect timeout in seconds
+            ConnectionOptions::OPTION_TIMEOUT => 3,
+            // whether or not to reconnect when a keep-alive connection has timed out on server
+            ConnectionOptions::OPTION_RECONNECT => true,
+            // optionally create new collections when inserting documents
+            ConnectionOptions::OPTION_CREATE => true,
+            // optionally create new collections when inserting documents
+            ConnectionOptions::OPTION_UPDATE_POLICY => UpdatePolicy::LAST,
+        );
+
+        $this->connection = new Connection($connectionOptions);
+    }
+
     public function autocomplete(string $teilwort)
     {
 
@@ -28,8 +59,6 @@ class ArangoAdapter implements WikiInterface
     public function shortestPath(string $start, string $end)
     {
 
-        # Verbindung zur Datenbank herstellen
-        $connection = self::getConnection();
         # ID zum Namen ermitteln: Hamburg -> pages/35d7df6ed3d93be2927d14acc5f1fc9a
         $start_id = self::findID($start);
         $ziel_id = self::findID($end);
@@ -47,7 +76,7 @@ class ArangoAdapter implements WikiInterface
 
         # Statement zur Ausführung der Query erzeugen
         $statement = new Statement(
-            $connection,
+            $this->connection,
             array(
                 "query" => $query,
                 "count" => true,
@@ -70,8 +99,7 @@ class ArangoAdapter implements WikiInterface
 
     public function randomEntry()
     {
-        # Verbindung aufbauen
-        $connection = self::getConnection();
+
 
         # AQL Query zur Selektion eines zufälligen Eintrages
         $query = "FOR doc IN @@collection
@@ -81,7 +109,7 @@ class ArangoAdapter implements WikiInterface
 
         # Statement zur Ausführung der Query erzeugen
         $statement = new Statement(
-            $connection,
+            $this->connection,
             array(
                 "query" => $query,
                 "count" => true,
@@ -102,8 +130,7 @@ class ArangoAdapter implements WikiInterface
 
     protected function findID($name)
     {
-        # Verbindung zu Datenbank herstellen
-        $connection = self::getConnection();
+
 
         $query = "  FOR doc IN @@collection
                     FILTER doc.`name` == @name
@@ -111,7 +138,7 @@ class ArangoAdapter implements WikiInterface
 
         # Statement zur Ausführung der Query erzeugen
         $statement = new Statement(
-            $connection,
+            $this->connection,
             array(
                 "query" => $query,
                 "count" => true,
@@ -129,31 +156,6 @@ class ArangoAdapter implements WikiInterface
 
     }
 
-    protected function getConnection()
-    {
 
-        $connectionOptions = array(
-            // server endpoint to connect to
-            ConnectionOptions::OPTION_ENDPOINT => 'tcp://127.0.0.1:8529',
-            // authorization type to use (currently supported: 'Basic')
-            ConnectionOptions::OPTION_AUTH_TYPE => 'Basic',
-            // user for basic authorization
-            ConnectionOptions::OPTION_AUTH_USER => 'root',
-            // password for basic authorization
-            ConnectionOptions::OPTION_AUTH_PASSWD => '',
-            // connection persistence on server. can use either 'Close' (one-time connections) or 'Keep-Alive' (re-used connections)
-            ConnectionOptions::OPTION_CONNECTION => 'Close',
-            // connect timeout in seconds
-            ConnectionOptions::OPTION_TIMEOUT => 3,
-            // whether or not to reconnect when a keep-alive connection has timed out on server
-            ConnectionOptions::OPTION_RECONNECT => true,
-            // optionally create new collections when inserting documents
-            ConnectionOptions::OPTION_CREATE => true,
-            // optionally create new collections when inserting documents
-            ConnectionOptions::OPTION_UPDATE_POLICY => UpdatePolicy::LAST,
-        );
-        return new Connection($connectionOptions);
-
-    }
 
 }
